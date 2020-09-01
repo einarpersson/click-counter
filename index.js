71879654d7a63b7cd841a825181b0484fb43318c
Einar Persson <me@einarpersson.com>
2020-09-01 22:18:24 +0200

    Counter

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Start writing your javascript here!
+const buttons = document.getElementsByTagName('button')
+const counter = document.getElementById('counter')
+
+let count = 0
+
+buttons[0].addEventListener('click', clickHandler)
+buttons[1].addEventListener('click', clickHandler)
+
+function clickHandler(ev) {
+  if (ev.target.innerText === '+') {
+    count++
+  } else if (ev.target.innerText === '-') {
+    count--
+  } else {
+    console.error('What just happened?')
+  }
+  counter.innerText = count
+}
